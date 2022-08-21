fb6f9c76bb7fdf990c05e6db4665548604af136d
radenfebri <febriye12@gmail.com>
2022-08-21 10:58:04 +0700

    fitur dark mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -33,3 +33,26 @@ window.addEventListener('click', function(e) {
         navMenu.classList.add('hidden');
     }
 });
+
+
+// Dark Mode Toogle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function() {
+    if (darkToggle.checked) {
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    } else {
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+    }
+});
+
+
+// Pidah posisi toggle sesuai mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+  } else {
+    darkToggle.checked = false;
+}
